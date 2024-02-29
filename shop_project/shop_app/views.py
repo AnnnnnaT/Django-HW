75fc5b49eba6a246042e439e0637787b807ed591
Anna <tka4evaanna2002@yande.ru>
2024-02-29 20:04:48 +0300

    changes in views added

--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -11,25 +11,36 @@ def orders_by(request, client_id):
     return render(request, 'shop_app/client_orders.html',  {'orders': orders})
 
 def products_by_date(request, client_id):
-    orders = Order.objects.filter(client_id = client_id)
-    week = []
-    month = []
-    year = []
+    # orders = Order.objects.filter(client_id = client_id)
+    # week = []
+    # month = []
+    # year = []
     products = {
-        "for_last_week": week,
-        "for_last_month": month,
-        "for_last_year": year
+        "for_last_week": last_week_orders,
+        "for_last_month": last_month_orders,
+        "for_last_year": last_year_orders
     }
-    for order in orders:
-        if order.order_date - datetime.datetime.now() < 7 or order.order_date - datetime.datetime.now() == 7:
-            for p in order.items:
-                week.append(p)
-        if order.order_date - datetime.datetime.now() < 31 or order.order_date - datetime.datetime.now() == 31:
-            for p in order.items:
-                if not p in week:
-                    month.append(p)
-        if order.order_date - datetime.datetime.now() < 365 or order.order_date - datetime.datetime.now() == 365:
-            for p in order.items:
-               if not p in week and not p in month:
-                    year.append(p)               
+
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    last_week_orders = Product.objects.filter(order__client=client_id,
+                                               order__order_date__gte=last_week).distinct() 
+    last_month = datetime.date.today() - datetime.timedelta(days=31)
+    last_month_orders = Product.objects.filter(order__client=client_id,
+                                               order__order_date__gte=last_month).distinct()
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    last_year_orders = Product.objects.filter(order__client=client_id,
+                                               order__order_date__gte=last_year).distinct() 
+
+    # for order in orders:
+    #     if order.order_date - datetime.datetime.now() < 7 or order.order_date - datetime.datetime.now() == 7:
+    #         for p in order.items:
+    #             week.append(p)
+    #     if order.order_date - datetime.datetime.now() < 31 or order.order_date - datetime.datetime.now() == 31:
+    #         for p in order.items:
+    #             if not p in week:
+    #                 month.append(p)
+    #     if order.order_date - datetime.datetime.now() < 365 or order.order_date - datetime.datetime.now() == 365:
+    #         for p in order.items:
+    #            if not p in week and not p in month:
+    #                 year.append(p)               
     return render(request, 'shop_app/products_in_order.html', products)
