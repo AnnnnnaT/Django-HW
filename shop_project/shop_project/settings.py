86f1b1833a9753c962d4e2fe067ce8fff1541255
Anna <tka4evaanna2002@yande.ru>
2024-03-12 13:52:12 +0300

    final changes

--- shop_project/shop_project/settings.py
+++ shop_project/shop_project/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv  
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ochsm39dtocpcgrd2xmh_-=ulw*v0ljmn=9aso491*2@8d)e3_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get("DEBUG") == "False":
+    DEBUG = False
+else:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True  
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = [["127.0.0.1", "gbshop.pythonanywhere.com"]]
 
 
 # Application definition
@@ -78,10 +89,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {  
+        "ENGINE": "django.db.backends.mysql",  
+        "NAME": os.getenv("MYSQL_DBNAME"),  
+        "USER": os.getenv("MYSQL_USER"),  
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),  
+        "HOST": os.getenv("MYSQL_HOST"),  
+        "OPTIONS": {  
+            "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",  
+            "charset": "utf8mb4",  
+        },  
+    }  
 }
 
 
@@ -120,8 +138,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static/"  
+
+MEDIA_URL = "media/"  
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NAME = os.environ.get("NAME")
+USER = os.environ.get("USER")
+PASSWORD = os.environ.get("PASSWORD")
+HOST = os.environ.get("HOST")
+
